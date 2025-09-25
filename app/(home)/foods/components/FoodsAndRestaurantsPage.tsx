ef2d98eb84a15b795411605f74d940a77ed18e69
DeveloperTWH <webdevelopment.twh@gmail.com>
2025-09-25 18:19:59 +0530

    25-09-2025
    1. Updated Desgin for food/food from shop to look like food from farm, according to rakesh sir.
    
    2. Resolved github size limit upload in in single push limit casued to .next folder

--- app/(home)/foods/components/FoodsAndRestaurantsPage.tsx
+++ app/(home)/foods/components/FoodsAndRestaurantsPage.tsx
@@ -8,13 +8,13 @@ export default function FoodsAndRestaurantsPage() {
   const [selectedTab, setSelectedTab] = useState('Food from Shop');
 
   const renderSection = () => {
-    if (selectedTab === 'Food from Shop') return <FoodFromShop />;
+    if (selectedTab === 'Food from Shop') return <FoodFromFarm />;
     if (selectedTab === 'Food from Farm') return <FoodFromFarm />;
     return <FoodFromRestaurant />;
   };
 
   return (
-    <div className="p-6 max-w-7xl mx-auto">
+    <div className="p-6 mx-auto max-w-7xl">
       <TabsHeadingSection selected={selectedTab} onTabChange={setSelectedTab} />
       {renderSection()}
     </div>
