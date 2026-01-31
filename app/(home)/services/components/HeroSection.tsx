69257aef83630bbce258d1fe0efa854a7c3a597c
SkFaizRahaman <skfaizrahaman@outlook.com>
2026-01-31 21:57:09 +0530

    services

--- app/(home)/services/components/HeroSection.tsx
+++ app/(home)/services/components/HeroSection.tsx
@@ -20,8 +20,11 @@ const HeroSection = ({ heading, imageUrl }: HeroSectionProps) => {
           {heading}
         </h1>
       </div> */}
-          <div className="absolute inset-0 flex items-center justify-center bg-black/40">
-        <h1 className="text-3xl font-bold leading-loose text-white md:text-5xl font-heading">
+      <div className="absolute inset-0 flex flex-col items-center justify-center bg-black/40">
+        <h1 className="text-3xl font-bold leading-loose text-white md:text-5xl font-poppins">
+          CHOOSE SERVICE
+        </h1>
+        <h1 className="text-base  leading-loose text-[#A2A2A2] font-poppins">
           Home // services
         </h1>
       </div>
