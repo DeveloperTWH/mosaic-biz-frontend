def53318dc862fb3cb77ee405a0011f52175fcca
satish donerao <doneraosatish@gmail.com>
2026-02-26 20:26:52 +0530

    registration vendor changes

--- app/(home)/partners/business/new/page.tsx
+++ app/(home)/partners/business/new/page.tsx
@@ -906,7 +906,7 @@ const handlePayAndSubmit = async () => {
                   value={form.primaryContactDesignation}
                   onChange={e => update('primaryContactDesignation', e.target.value)}
                 >
-                  <option value="">-- Choose Your Designation --</option>
+                  <option value="">-- Select Designation --</option>
                   <option value="Owner">Owner</option>
                   <option value="Manager">Manager</option>
                   <option value="Director">Director</option>
@@ -971,15 +971,65 @@ const handlePayAndSubmit = async () => {
                 />
               </InputField>
 
-              <InputField label="State" required error={formErrors.address_state}>
-                <input
-                  type="text"
-                  className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-colors"
-                  value={form.address.state}
-                  onChange={e => updateAddress('state', e.target.value)}
-                  placeholder="State"
-                />
-              </InputField>
+<InputField label="State">
+  <select
+    className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-colors bg-white"
+    value={form.address.state}
+    onChange={e => updateAddress('state', e.target.value)}
+  >
+    <option value="">Select State</option>
+    <option value="Alabama">Alabama</option>
+    <option value="Alaska">Alaska</option>
+    <option value="Arizona">Arizona</option>
+    <option value="Arkansas">Arkansas</option>
+    <option value="California">California</option>
+    <option value="Colorado">Colorado</option>
+    <option value="Connecticut">Connecticut</option>
+    <option value="Delaware">Delaware</option>
+    <option value="Florida">Florida</option>
+    <option value="Georgia">Georgia</option>
+    <option value="Hawaii">Hawaii</option>
+    <option value="Idaho">Idaho</option>
+    <option value="Illinois">Illinois</option>
+    <option value="Indiana">Indiana</option>
+    <option value="Iowa">Iowa</option>
+    <option value="Kansas">Kansas</option>
+    <option value="Kentucky">Kentucky</option>
+    <option value="Louisiana">Louisiana</option>
+    <option value="Maine">Maine</option>
+    <option value="Maryland">Maryland</option>
+    <option value="Massachusetts">Massachusetts</option>
+    <option value="Michigan">Michigan</option>
+    <option value="Minnesota">Minnesota</option>
+    <option value="Mississippi">Mississippi</option>
+    <option value="Missouri">Missouri</option>
+    <option value="Montana">Montana</option>
+    <option value="Nebraska">Nebraska</option>
+    <option value="Nevada">Nevada</option>
+    <option value="New Hampshire">New Hampshire</option>
+    <option value="New Jersey">New Jersey</option>
+    <option value="New Mexico">New Mexico</option>
+    <option value="New York">New York</option>
+    <option value="North Carolina">North Carolina</option>
+    <option value="North Dakota">North Dakota</option>
+    <option value="Ohio">Ohio</option>
+    <option value="Oklahoma">Oklahoma</option>
+    <option value="Oregon">Oregon</option>
+    <option value="Pennsylvania">Pennsylvania</option>
+    <option value="Rhode Island">Rhode Island</option>
+    <option value="South Carolina">South Carolina</option>
+    <option value="South Dakota">South Dakota</option>
+    <option value="Tennessee">Tennessee</option>
+    <option value="Texas">Texas</option>
+    <option value="Utah">Utah</option>
+    <option value="Vermont">Vermont</option>
+    <option value="Virginia">Virginia</option>
+    <option value="Washington">Washington</option>
+    <option value="West Virginia">West Virginia</option>
+    <option value="Wisconsin">Wisconsin</option>
+    <option value="Wyoming">Wyoming</option>
+  </select>
+</InputField>
 
               <InputField label="Country">
                 <select
@@ -987,11 +1037,11 @@ const handlePayAndSubmit = async () => {
                   value={form.address.country}
                   onChange={e => updateAddress('country', e.target.value)}
                 >
-                  <option value="">Select Country</option>
                   <option value="USA">United States</option>
-                  <option value="Canada">Canada</option>
+                  {/* <option value="USA">United States</option> */}
+                  {/* <option value="Canada">Canada</option>
                   <option value="UK">United Kingdom</option>
-                  <option value="Australia">Australia</option>
+                  <option value="Australia">Australia</option> */}
                 </select>
               </InputField>
 
@@ -1018,7 +1068,7 @@ const handlePayAndSubmit = async () => {
                   value={form.businessOwnershipType}
                   onChange={e => update('businessOwnershipType', e.target.value as OwnershipType)}
                 >
-                  <option value="">-- Choose Your Designation --</option>
+                  <option value="">-- Select Designation --</option>
                   <option value="Limited Liability Company">Limited Liability Company (LLC)</option>
                   <option value="Sole Proprietor">Sole Proprietor</option>
                   <option value="S-Corporation">S-Corporation</option>
