52155bfd7ef6184387c5ceed45e23de407b33c55
Sk Faiz Rahaman <skfaizrahaman@outlook.com>
2026-05-12 22:12:26 +0530

    buy now button commneted out

--- app/(home)/product/[id]/page.tsx
+++ app/(home)/product/[id]/page.tsx
@@ -866,7 +866,9 @@ setMainImage(firstImage);
                 </button>
               )}
 
-              <button
+
+              {/* uncomment later */}
+              {/* <button
                 className="flex-1 py-2.5 font-semibold text-white bg-[#c79b44] rounded hover:bg-[#b08a3a] transition-colors text-sm uppercase tracking-wide disabled:opacity-50 disabled:cursor-not-allowed"
                 disabled={!isVariantSelected() || Boolean(selectedVariant && selectedVariant.stock <= 0)}
                 onClick={() => {
@@ -882,7 +884,7 @@ setMainImage(firstImage);
                 }}
               >
                 Buy Now
-              </button>
+              </button> */}
             </div>
 
             {/* Product Details Table — RIGHT COLUMN */}
